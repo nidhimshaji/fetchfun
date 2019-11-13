96b4ffd7a8f51405821cd6e3ca26af0b796199c5
nidhimshaji <42461490+nidhimshaji@users.noreply.github.com>
2019-11-12 21:51:12 -0800

    Update useFetch.js

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,25 +1,4 @@
-// import { useCallback, useState } from "react";
 
-// const useFetch = url => {
-//   const [resData, setresData] = useState();
-
-//   const getData = useCallback(url => {
-//     fetch(url)
-//       .then(res => res.json())
-//       .then(data => {
-//         // console.log("data", data);
-//         setresData(data);
-//       });
-//   }, []);
-
-//   const invokeFetch = url => {
-//     getData(url);
-//   };
-
-//   return { invokeFetch };
-// };
-
-// export default useFetch;
 
 export function fetchAPI(url) {
   // param is a highlighted word from the user before it clicked the button
